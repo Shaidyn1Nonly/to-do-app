8138df372958ba439e878cda63c6542407accf89
Shaidyn1Nonly <shaidynd@gmail.com>
2017-12-16 21:59:45 -0500

    Checkpoint 7 Simple-To-Do App Part-1

---
+++ app.js
@@ -0,0 +1,38 @@
+function onReady(){
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+    addToDoForm.addEventListener('submit', event => {
+    //default is to reload window after SUBMIT don't do that.
+    event.preventDefault();
+    //create title variable - we will have title variable will equal the value of the input entered when SUBMIT event gets activated
+    //get the text
+    let title = newTodoText.value;
+
+    //create a new li
+    let newLi = document.createElement('li');
+
+    //create a new input
+    let checkbox = document.createElement('input');
+    //set the input's type to checkbox
+    checkbox.type = 'checkbox';
+
+    //set the title
+    newLi.textContent = title;
+
+    //attached the checkbox to the Li
+    newLi.appendChild(checkbox);
+
+    //attach the li to the ul
+    toDoList.appendChild(newLi);
+
+    //with this we will clear out input field as a courtesy to user
+    //empty the input
+    newToDoText.value = '';
+});
+}
+
+window.onload = function(){
+  onReady();
+};

3740620c001672e3cb73f2c7b3cfb11064404e49
Shaidyn1Nonly <shaidynd@gmail.com>
2017-12-23 16:38:48 -0500

    create rendeTheUi function for the TodoApp and refractored previous toDoApp with createNewToDo(), update UI by the state

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 function onReady(){
+  let toDos =[];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  const newToDoText= document.getElementById('newToDoText');
 
-    addToDoForm.addEventListener('submit', event => {
-    //default is to reload window after SUBMIT don't do that.
+  //create function for the new to do
+  function createNewToDo(){
+    if (!newToDoText.value) { return;}
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    newToDoText.value = " "; //manually setting to clear
+    renderTheUI();
+    }
+
+    //render is needed before the event of submit
+    //will take out current state in the TodoArray and render the UI
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = "";
+    //we'll use forEach to loop through array & render each to-do as an li
+    //in the to-do <ul> list
+    toDos.forEach(function(toDo){
+     const newLi = document.createElement('li');
+     const checkbox = document.createElement('input');
+     checkbox.type= "checkbox";
+
+     newLi.textContent = toDo.title;
+
+     toDoList.appendChild(newLi);
+     newLi.appendChild(checkbox);
+      });
+    }
+
+   //create event listener for form
+   addToDoForm.addEventListener('submit', event =>{
     event.preventDefault();
+<<<<<<< HEAD
     //create title variable - we will have title variable will equal the value
     //of the input entered when SUBMIT event gets activated
     //get the text
@@ -32,6 +65,17 @@ function onReady(){
     //empty the input
     newToDoText.value = '';
   });
+=======
+    createNewToDo();
+  });
+  /*we are capable of creating todo and can listen to the submit event
+  and state if changing we can't see it in our UI. we need to USE our state
+  to render the UI.
+  strategy will be to create a function called renderTheUI to render
+  based on current state. when first load render that state, which is an empty
+  value*/
+  renderTheUI(); //renders when listens for submit event on form
+>>>>>>> checkpoint-8-simple-to-do
 }
 
 window.onload = function(){

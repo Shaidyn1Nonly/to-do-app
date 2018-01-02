8e68d03bb550952c7640d7f9bdf088cf13f91cd8
Shaidyn1Nonly <shaidynd@gmail.com>
2018-01-02 12:16:24 -0500

    add functionality to delete button

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 function onReady(){
+  let id = 0;
   let toDos =[];
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText= document.getElementById('newToDoText');
 
   //create function for the new to do
   function createNewToDo(){
-    if (!newToDoText.value) { return;}
+     if (!newToDoText.value) { return;}
 
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
+     toDos.push({
+       title: newToDoText.value,
+       complete: false,
+       id: id
+     });
 
-    newToDoText.value = " "; //manually setting to clear
-    renderTheUI();
+     id++; //increment the id var to count give ID# to toDo
+
+     newToDoText.value = " "; //manually setting to clear
+     renderTheUI();
     }
 
     //render is needed before the event of submit
@@ -23,30 +27,42 @@ function onReady(){
     toDoList.textContent = "";
     //we'll use forEach to loop through array & render each to-do as an li
     //in the to-do <ul> list
-    toDos.forEach(function(toDo){
-     const newLi = document.createElement('li');
-     const checkbox = document.createElement('input');
-     checkbox.type= "checkbox";
 
-     newLi.textContent = toDo.title;
+        toDos.forEach(function(toDo){
+        const newLi = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type= "checkbox";
 
-     toDoList.appendChild(newLi);
-     newLi.appendChild(checkbox);
-      });
-    }
+        newLi.textContent = toDo.title;
+
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = "Delete to do!";
+        newLi.appendChild(deleteButton);
+
+             deleteButton.addEventListener('click', event =>{
+                toDoList.removeChild(newLi);
+                function wasDeleted(removedItem){
+                return removedItem.id != toDo.id;
+               }
+               toDos = toDos.filter(wasDeleted);
+             });
+        });
+      }
 
    //create event listener for form
    addToDoForm.addEventListener('submit', event =>{
     event.preventDefault();
     createNewToDo();
-  });
-  /*we are capable of creating todo and can listen to the submit event
-  and state if changing we can't see it in our UI. we need to USE our state
-  to render the UI.
-  strategy will be to create a function called renderTheUI to render
-  based on current state. when first load render that state, which is an empty
-  value*/
-  renderTheUI(); //renders when listens for submit event on form
+   });
+   /*we are capable of creating todo and can listen to the submit event
+   and state if changing we can't see it in our UI. we need to USE our state
+   to render the UI.
+   strategy will be to create a function called renderTheUI to render
+   based on current state. when first load render that state, which is an empty
+   value*/
+   renderTheUI(); //renders when listens for submit event on form
 }
 
 window.onload = function(){
